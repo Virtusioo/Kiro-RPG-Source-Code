d92038df524d40e9373bb7a742182012751c81cd
unknown <virtusiodavidrafael7.snfss@gmail.com>
2025-07-26 20:37:16 +0800

    Improved JSON example

--- src/common/file.c
+++ src/common/file.c
@@ -4,12 +4,13 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
 
 char* file_read_all(const char* path)
 {
-    FILE* file = fopen(path, "r");
+    FILE* file = fopen(path, "rb");
     if (file == NULL)
-        common_panic("file_read_all(\"%s\") failed: %s", strerror(errno));
+        common_panic("file_read_all(\"%s\") failed: %s", path, strerror(errno));
 
     fseek(file, 0, SEEK_END);
     size_t size = ftell(file);
@@ -18,8 +19,12 @@ char* file_read_all(const char* path)
     char* buffer = common_malloc(size + 1); 
 
     size_t read = fread(buffer, 1, size, file);
-    buffer[read] = '\0';
+    if (read != size) {
+        fclose(file);
+        common_panic("fread() failed: expected %zu bytes, got %zu", size, read);
+    }
 
+    buffer[size] = '\0';
     fclose(file);
     return buffer;
 }
--- src/kiro/main.c
+++ src/kiro/main.c
@@ -1,23 +1,30 @@
 
 #include "json/json.h"
+#include "common/file.h"
 
 #include <stdio.h>
 
 int main()
 {
-    JsonResult result = json_parse("{\"key\": [1, 2, 3, 4]}");
+    char* path = "build/map.tmj";
+    char* content = file_read_all(path);
+    JsonResult result = json_parse(content);
 
     if (result.errors == NULL) {
         JsonValue* value = result.value;
-        JsonValue* array = json_objectget(value, "key");
+        JsonValue* layers = json_objectget(value, "layers");
 
-        for (size_t i = 0; i < json_arraylen(array); i++) {
-            JsonValue* json_num = json_arrayget(array, i);
-            float number = json_getnumber(json_num);
-            printf("%f\n", number);
+        for (size_t i = 0; i < json_arraylen(layers); i++) {
+
+            JsonValue* layer = json_arrayget(layers, i);
+            JsonValue* name = json_objectget(layer, "name");
+            const char* layer_name = json_getstring(name);
+
+            printf("%s\n", layer_name);
         }
+
     } else {
-        printf("%s", result.errors);
+        printf("In .json file '%s':\n%s", path, result.errors);
     }
 
     json_destroyresult(&result);
