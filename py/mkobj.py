cd96cea039691625198f6c87561fcdf107390161
unknown <virtusiodavidrafael7.snfss@gmail.com>
2025-07-30 20:27:35 +0800

    Removed main() for less verbosity

--- py/mkobj.py
+++ py/mkobj.py
@@ -5,13 +5,9 @@ import sys
 SRC_DIR = sys.argv[1]
 OBJ_DIR = 'obj'
 
-def main():
-    for root, dirs, _ in os.walk(SRC_DIR):
-        for d in dirs:
-            src_path = os.path.join(root, d)
-            rel_path = os.path.relpath(src_path, SRC_DIR)
-            obj_path = os.path.join(OBJ_DIR, rel_path)
-            os.makedirs(obj_path, exist_ok=True)
-
-if __name__ == '__main__':
-    main()
+for root, dirs, _ in os.walk(SRC_DIR):
+    for d in dirs:
+        src_path = os.path.join(root, d)
+        rel_path = os.path.relpath(src_path, SRC_DIR)
+        obj_path = os.path.join(OBJ_DIR, rel_path)
+        os.makedirs(obj_path, exist_ok=True)
